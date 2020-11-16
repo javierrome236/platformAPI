c2bbc21b0da7a342df6593701b4907959cc9c5ed
javierrome236 <37107269+javierrome236@users.noreply.github.com>
2020-11-16 12:16:30 +0100

    Add files via upload

---
+++ swagger_server/test/test_statistics_controller.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.device import Device  # noqa: E501
+from swagger_server.models.position import Position  # noqa: E501
+from swagger_server.models.statistics import Statistics  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestStatisticsController(BaseTestCase):
+    """StatisticsController integration test stubs"""
+
+    def test_get_activity_statistics(self):
+        """Test case for get_activity_statistics
+
+        Devuelve las estadísticas de actividad de un usuario
+        """
+        response = self.client.open(
+            '/javierrome/PlatformAPI/1.0.0/statistics/{userId}'.format(user_id=56),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_live_time_position(self):
+        """Test case for get_live_time_position
+
+        Devuelve posición actual del dispositivo
+        """
+        response = self.client.open(
+            '/javierrome/PlatformAPI/1.0.0/statistics/device/liveTime/{deviceId}'.format(device_id=56),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_post_device(self):
+        """Test case for post_device
+
+        añade un nuevo dispositivo a la cuenta
+        """
+        body = Device()
+        response = self.client.open(
+            '/javierrome/PlatformAPI/1.0.0/statistics/device/{deviceId}'.format(device_id=56),
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
---
+++ swagger_server/test/test_user_controller.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.user import User  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestUserController(BaseTestCase):
+    """UserController integration test stubs"""
+
+    def test_create_user(self):
+        """Test case for create_user
+
+        Crea un nuevo usuario en el sistema
+        """
+        body = User()
+        response = self.client.open(
+            '/javierrome/PlatformAPI/1.0.0/user',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_user_by_name(self):
+        """Test case for get_user_by_name
+
+        Obtiene el usuario por el nombre de usuario
+        """
+        response = self.client.open(
+            '/javierrome/PlatformAPI/1.0.0/user/{username}'.format(username='username_example'),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_update_user(self):
+        """Test case for update_user
+
+        Actualiza los datos del usuario
+        """
+        body = User()
+        response = self.client.open(
+            '/javierrome/PlatformAPI/1.0.0/user/{username}'.format(username='username_example'),
+            method='PUT',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
