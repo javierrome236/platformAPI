8eec1f0f1c5d7b537fd040e811b41f826878ca82
javierrome236 <37107269+javierrome236@users.noreply.github.com>
2020-11-16 12:26:52 +0100

    Add files via upload

---
+++ swagger_server/controllers/user_controller.py
@@ -0,0 +1,50 @@
+import connexion
+import six
+
+from swagger_server.models.user import User  # noqa: E501
+from swagger_server import util
+
+
+def create_user(body):  # noqa: E501
+    """Crea un nuevo usuario en el sistema
+
+     # noqa: E501
+
+    :param body: Created user object
+    :type body: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        body = User.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def get_user_by_name(username):  # noqa: E501
+    """Obtiene el usuario por el nombre de usuario
+
+     # noqa: E501
+
+    :param username: nombre de usuario
+    :type username: str
+
+    :rtype: User
+    """
+    return 'do some magic!'
+
+
+def update_user(body, username):  # noqa: E501
+    """Actualiza los datos del usuario
+
+     # noqa: E501
+
+    :param body: Usuario actualizado
+    :type body: dict | bytes
+    :param username: nombre del usuario que va a ser actualizado
+    :type username: str
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        body = User.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
---
+++ swagger_server/models/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.device import Device
+from swagger_server.models.position import Position
+from swagger_server.models.statistics import Statistics
+from swagger_server.models.user import User
